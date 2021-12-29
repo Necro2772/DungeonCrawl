39232b83dff0b105d57dde7e32577325c5a103e3
Michael Roberts <50212290+Necro2772@users.noreply.github.com>
2021-12-29 11:05:14 -0800

    typo

--- DungeonTest/src/Dungeon/WebController.java
+++ DungeonTest/src/Dungeon/WebController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class WebController {
 
